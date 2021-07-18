82fff6834c2720d32b8171ab65864dda0fe4a4f4
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-07-18 15:21:31 -0400

    heroku fix #3

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const passport = require('passport');
 require('./config/passport')(passport);
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
