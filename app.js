d242e2bbeafc68fdc254b1a81e9014283ad3bbe4
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-07-18 18:20:33 -0400

    added twitter api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require("express");
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
+const twitter = require("./routes/api/twitter");
 const User = require('./models/User');
 const passport = require('passport');
 require('./config/passport')(passport);
@@ -10,9 +11,9 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
-  }
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -25,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/users", users);
+app.use("/api/tweets", twitter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
+import { fetchTweets } from './util/twitter_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
@@ -29,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
         store = configureStore({});
     }
     window.store = store
+    window.tweets = fetchTweets
     const root = document.getElementById('root');
 
     ReactDOM.render(<Root store={store} />, root);
