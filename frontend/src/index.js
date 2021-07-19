add418c9b9acb95df38cd4450918e083f373c86b
GT458 <isohrob1@gmail.com>
2021-07-18 23:15:52 -0400

    move reset file into src folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
-
+import './html5reset-1.6.1.scss';
 document.addEventListener('DOMContentLoaded', () => {
     let store;
 
