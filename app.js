d40b50216d2b78aec1ba08fcd0b6b8a72bd66c07
GT458 <isohrob1@gmail.com>
2021-07-18 22:26:09 -0400

    add css reset file

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const User = require('./models/User');
 const passport = require('passport');
 require('./config/passport')(passport);
 const app = express();
-
+// const path = require('path');
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
-  }
+  } 
 
 mongoose
     .connect(db, { useNewUrlParser: true })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -32,4 +32,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
 
     ReactDOM.render(<Root store={store} />, root);
+    // ReactDOM.render(<h1>Hello</h1>, root);
 });
