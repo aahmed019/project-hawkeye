7446dd892b3cc90cf2a60497d06ed018dd809517
Jack Liu <Jack.tf.liu@gmail.com>
2021-07-19 10:33:13 -0400

    splash page skeleton

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import './main.scss'
 
 class MainPage extends React.Component {
 
     render() {
         return (
-        <div>
-            <h1>Hawkeye</h1>
+        <div className='splash-container'>
+
+            <div className='under-nav'>
+                <div className='splash-words'>
+                    <h1 className='nav-header'>HEADER</h1>
+                    <h2 className='nav-paragraph'>PARAGRAPH</h2>
+                </div>
+            </div>
+            {/* section 1  */}
+            <div className='section1'>
+                <div className='section1-box1'>
+                    WORDS PARAGRAPH
+                </div>
+
+                <div className='section1-box2'>
+                Box2 PHOTO
+                </div>
+            </div>
+
+            {/* section 2 */}
+            <div className='section2'>
+                <div className='section2-box1'>
+                Box2 PHOTO
+                </div>
+
+                <div className='section2-box2'>
+                    WORDS PARAGRAPH
+                </div>
+            </div>
+
+            {/* section 3 */}
+            <div className='section3'>
+                <div className='section3-box1'>
+                    WORDS PARAGRAPH
+                </div>
+
+                <div className='section3-box2'>
+                Box2 PHOTO
+                </div>
+            </div>
+            
         </div>
         );
     }
