94a95399ab36c72118e135e7a2543b5e4225ecc9
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-07-19 12:09:27 -0400

    testing with twitter-v2

--- routes/api/twitter.js
+++ routes/api/twitter.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const router = express.Router();
-var Twitter = require('twitter');
+const Twitter = require('twitter-v2');
 require('dotenv').config();
 
 var client = new Twitter({
+    bearer_token: process.env.BEARER_TOKEN,
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
-    bearer_token: process.env.BEARER_TOKEN,
 });
 
 router.get('/', (req, res) => {
 
     var params = {
-        q: 'POTUS',
-        count: 2
+        screen_name: 'POTUS'
     }
 
-    return client.get('search/tweets', params, function(error, data, response) {
-        let tweets = data.statuses
-        let results = {}
-        for (let i = 0; i < tweets.length; i++) {
-            results[i] = tweets[i].text
+    client.get('users/show', params, function(error, user, response){
+        if (!error) {
+            client.get(`2/users/${user.id_str}/tweets`, { }, function(error, data, response) {
+                console.log(data)
+                // let tweets = data.statuses
+                // let results = {}
+                // for (let i = 0; i < tweets.length; i++) {
+                //     results[i] = tweets[i].text
+                // }
+                // res.json(results);
+            })
         }
-        res.json(results);
-    })
+        else{
+            console.log(error)
+        }
+    });
+
+    // client.get('statuses/user_timeline', { screen_name: `POTUS`, q:'children',  count: 1, exclude_replies: true, include_rts: false}, function(error, data, response) {
+    //         console.log(data)
+    //         let tweets = data.statuses
+    //         let results = {}
+    //         for (let i = 0; i < tweets.length; i++) {
+    //             results[i] = tweets[i].text
+    //         }
+    //         res.json(results);
+    // })
+
 });
 
 
