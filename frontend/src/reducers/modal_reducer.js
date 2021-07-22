4e6ed5f87cadf4d956071289aed6f9d9edaf3c50
Vincent Ahn <jmahn97@hotmail.com>
2021-07-22 16:41:15 -0400

    Fixed issue where tweet wasn't being removed

--- frontend/src/reducers/modal_reducer.js
+++ frontend/src/reducers/modal_reducer.js
@@ -1,12 +1,22 @@
 import { OPEN_MODAL, CLOSE_MODAL } from "../actions/modal_actions";
+import { REMOVE_TWEET_FROM_FOLDER } from "../actions/workspace_actions";
 
 export default function(state = null, action){
   Object.freeze(state);
+  let newState = Object.assign({}, state);
+
   switch(action.type) {
     case OPEN_MODAL:
       return action.data;
     case CLOSE_MODAL:
       return null;
+    case REMOVE_TWEET_FROM_FOLDER:
+      debugger;
+      if(!(state && state.folder)) return state;
+      let newTweets = [...newState.folder.tweets];
+      newTweets.splice(action.tweetIdx, 1);
+      newState.folder.tweets = newTweets;
+      return newState;
     default:
       return state;
   }
