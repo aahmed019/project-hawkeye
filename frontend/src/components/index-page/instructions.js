e98524ece1c7f905bd069b7bb1955721b125440f
GT458 <isohrob1@gmail.com>
2021-07-22 09:27:44 -0400

    modal styling

--- frontend/src/components/index-page/instructions.js
+++ frontend/src/components/index-page/instructions.js
@@ -1,23 +1,25 @@
 
 
 export const Instructions = () => (
-   <div>
-            <h1>Instructions</h1>
-          <div>
-            <p>
-            Searching and filtering Tweets
+   <div className='instructions-wrapper'>
+     <div className='instructions-header'>
+        <h1>Get Started</h1>
+     </div>
+        <div className='instructions-body'>
+          <p>
+          Searching and filtering Tweets
 
-            Use twitter usernames and filter words to find tweets containing the words you want to appear in the tweet. 
-            Clicking on individual tweets will redirect you to one's actual tweet once the results are loaded. 
-            </p>
+          Use twitter usernames and filter words to find tweets containing the words you want to appear in the tweet. 
+          Clicking on individual tweets will redirect you to one's actual tweet once the results are loaded. 
+          </p>
 
-            <p>
-            Creating workspaces and Folders
+          <p>
+          Creating workspaces and Folders
 
-            Workspaces can be customized on the side. 
-            Creating a workspace allows you to create custom folders for saving your tweet results. 
-            </p>
-        </div>
+          Workspaces can be customized on the side. 
+          Creating a workspace allows you to create custom folders for saving your tweet results. 
+          </p>
+      </div>
     </div>
 );
 
