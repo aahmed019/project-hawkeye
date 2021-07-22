75afda4d4a5a65250dee7723a2ad2a8286ba7e19
Jack Liu <Jack.tf.liu@gmail.com>
2021-07-22 12:05:36 -0400

    added tweets to folder modal

--- frontend/src/components/modals/open_folder_modal.js
+++ frontend/src/components/modals/open_folder_modal.js
@@ -23,15 +23,35 @@ class CreateWorkspaceModal extends React.Component {
 
   render() {
     if (!this.props.modal || this.props.modal.modalType !== 'open_folder') return null;
-
+    const tweets = this.props.modal.folder.tweets
+    const hello = tweets.map((tweet, idx) => (
+      <li className='tweet' key={`tweet-${idx}`}>
+        <ul>
+          <li className='tweet-head'>
+            <li className='tweet-img'><img src={tweet.profile_pic} /></li>
+            <a className='tweet-link'href={tweet.user_url}>
+              {tweet.username}
+            </a>
+          </li>
+          <li className='tweet-body'>
+            <a href={tweet.source}>
+              {tweet.text}
+            </a>
+          </li>
+        </ul>
+      </li>
+    ))
+    
     return (
       <div className='modal-folder'>
         <div className='modal-folder-child'>
+          <span className='close-button'><button onClick={() => this.props.closeModal()}>&#x2715;</button></span>
           <div className='modal-form-folder'>
-            <span className='close-button'><button onClick={() => this.props.closeModal()}>&#x2715;</button></span>
             <div className='folder-container'>
               <div className='left-tweet'>
-
+                <div className='tweet-container'>
+                  {hello}  
+                </div>
               </div>
               <div className='right-comments'>
                 <Comments/>
