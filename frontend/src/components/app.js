7a9da077945538e6c2359ce37ac36f223fd63966
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-07-22 00:44:59 -0400

    fixed modal issues

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,7 +8,6 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-import Workspace from './workspace/workspace';
 import MainIndex from './index-page/main-index';
 import CreateWorkspaceModal from './modals/create_workspace_modal';
 import OpenFolderModal from './modals/open_folder_modal';
@@ -22,10 +21,9 @@ const App = () => (
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
         </Switch>
-        <Switch>
             <AuthRoute exact path="/" component={MainPage} />
             <ProtectedRoute path="/" component={MainIndex} />
-        </Switch>
+        
     </div>
 );
 export default App;
