24a45a697bb65ab216fb278aa71ef692135ed2ca
Vincent Ahn <jmahn97@hotmail.com>
2021-08-09 15:26:34 -0400

    Update index and store
    Index previously redirected to login when cookie expires instead of home
    page.
    
    Store properly only includes logger if build isn't production build.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,8 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const currentTime = Date.now() / 1000;
 
         if (decodedUser.exp < currentTime) {
-        store.dispatch(logout());
-        window.location.href = '/login';
+          store.dispatch(logout());
         }
     } else {
         store = configureStore({});
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,11 +4,18 @@ import logger from 'redux-logger';
 
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if(process.env.NODE_ENV !== "production"){
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(thunk, logger)
+        applyMiddleware(...middlewares)
     )
 );
 
